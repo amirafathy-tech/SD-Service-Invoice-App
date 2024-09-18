a67f6484f40f09dc9257854ef35ea7bcf9a88940
amirafathy-tech <amira.fathy@solex.tech>
2024-09-18 17:22:50 +0300

    update-environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
-    production: true,
-   // apiUrl: 'https://sd-dev.c-878bd9e.kyma.ondemand.com',
-   apiUrl: 'https://sd.c-8a1e857.kyma.ondemand.com',
-    clientID: "f73c32fb-86bc-4908-afb7-ee0a7144a67b",
-    clientSecret: "o7nRa6E3nXPQvwa=CUOQj1o5cBzLe:x"
-  };
+  production: true,
+  apiUrl: 'https://sd.c-8a1e857.kyma.ondemand.com',
+  clientID: "f73c32fb-86bc-4908-afb7-ee0a7144a67b",
+  clientSecret: "o7nRa6E3nXPQvwa=CUOQj1o5cBzLe:x"
+};
